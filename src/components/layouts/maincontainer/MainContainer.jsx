911e8076ba2b44732e8aa087a8d31f28913a19cb
mdev79 <michal.tyszko@gmail.com>
2019-10-30 20:42:09 +0100

    cleaning props

--- src/components/layouts/maincontainer/MainContainer.jsx
+++ src/components/layouts/maincontainer/MainContainer.jsx
@@ -3,18 +3,25 @@ import GenerateBtn from './GenerateBtn';
 import OptionsContainer from './options/OptionsContainer';
 
 const MainContainer = props => {
-  const {options,
-    howManyChars,
-    smallLetters,
-    bigLetters,
-    numbers,
-    specialChars,} = props
   return (
     <div className='main__container'>
       <GenerateBtn mainBtn={props.mainBtn} />
-      <OptionsContainer options={options} howManyChars={howManyChars} smallLetters={smallLetters} bigLetters={bigLetters} numbers={numbers} specialChars={specialChars} />
+      <OptionsContainer {...props} />
     </div>
   );
 };
 
 export default MainContainer;
+
+//  previous version of props in component:
+
+// const {
+//   options,
+//   howManyChars,
+//   smallLetters,
+//   bigLetters,
+//   numbers,
+//   specialChars
+// } = props;
+
+// <OptionsContainer options={options} howManyChars={howManyChars} smallLetters={smallLetters} bigLetters={bigLetters} numbers={numbers} specialChars={specialChars} />
